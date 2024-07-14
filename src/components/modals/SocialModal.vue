6afcf502a6d3a3e7ee9001838c58cd2ec5db5d64
Ash Hellwig <ahellwig.dev@gmail.com>
2024-07-14 04:01:42 -0600

    fix social modal design

--- src/components/modals/SocialModal.vue
+++ src/components/modals/SocialModal.vue
@@ -5,8 +5,12 @@ export default {
       open: false,
       socials: [
         {
-          name: 'Instagram',
-          icon: ''
+          aria: 'LinkTree',
+          icon: 'fa-solid fa-link',
+          link: 'https://linktr.ee/ashwig',
+          target: '_blank',
+          rel: 'noreferrer',
+          username: 'ashwig'
         }
       ]
     }
@@ -20,11 +24,58 @@ export default {
   <ui-dialog v-model="open">
     <ui-dialog-title closable>Follow Me!</ui-dialog-title>
     <ui-dialog-content>
-      <ui-list>
-        <ui-item v-for="(social, index) in socials" :key="index">{{
-          social.name
-        }}</ui-item>
+      <ui-list :type="2">
+        <ui-nav-item
+          v-for="(social, index) in socials"
+          :key="index"
+          :href="social.link"
+          :target="social.target"
+          :rel="social.rel"
+        >
+          <ui-item-first-content>
+            <font-awesome-icon :icon="social.icon"></font-awesome-icon>
+          </ui-item-first-content>
+          <ui-item-text-content>
+            <ui-item-text1>
+              {{ social.aria }}
+            </ui-item-text1>
+            <ui-item-text2>({{ social.username }})</ui-item-text2>
+          </ui-item-text-content>
+        </ui-nav-item>
       </ui-list>
     </ui-dialog-content>
   </ui-dialog>
 </template>
+
+<style>
+#app
+  > main
+  > div:nth-child(2)
+  > div
+  > h3
+  > div
+  > div.mdc-dialog__container
+  > div.mdc-dialog__surface {
+  background-color: var(--vt-c-black-ish-opaque) !important;
+}
+
+.mdc-deprecated-list-item__graphic {
+  color: var(--mdc-theme-primary) !important;
+}
+
+.mdc-dialog__title {
+  color: var(--mdc-theme-primary) !important;
+}
+
+.mdc-dialog__close {
+  color: var(--mdc-theme-primary) !important;
+}
+
+.mdc-deprecated-list-item__primary-text {
+  color: var(--mdc-theme-primary) !important;
+}
+
+.mdc-deprecated-list-item__secondary-text {
+  color: var(--mdc-theme-primary-dark) !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,47 @@
+// Import Vue
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import BalmUI
 import BalmUI from 'balm-ui'
 import BalmUIPlus from 'balm-ui-plus'
 import 'balm-ui-css'
+
+// Import local CSS
 import './assets/main.css'
 
+// Import FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faTwitch,
+  faYoutube
+} from '@fortawesome/free-brands-svg-icons'
+
+// Create the app
 const app = createApp(App)
 
+// Add the FontAwesome library
+library.add(
+  faGithub,
+  faInstagram,
+  faLink,
+  faLinkedin,
+  faTwitter,
+  faTwitch,
+  faYoutube
+)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+// Add BalmUI
 app.use(BalmUI)
 app.use(BalmUIPlus)
 
+// Mount app
 app.mount('#app')
